f2dfd376acfebe09e75d373c988424505e78d882
se05503 <seyeongb18@gmail.com>
2026-09-09 14:53:11 +0900

    feat: GATT 서비스 및 특성 기반 BLE 통신 기본 로직 - 데이터 송수신을 위한 GATT(Service, Characteristic) UUID 생성 - BLE 기기 이름 초기화 - BLE 서버, 서비스, 특성 생성 - 서비스 개시 - 패킷을 전파 형태로 공기중에 송신 → 스마트폰이 블루투스 탐지 가능

---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+
+// 기기간 연결 후 데이터 송수신을 위한 GATT 고유번호
+#define SERVICE_UUID "4fa1c432-132e-4b77-b952-b91b34c25638"
+#define CHARACTERISTIC_UUID "6e400002-b5a3-f393-e0a9-e50e24dcca9e"
+
+void setup()
+{
+    // 1. BLE 기기 이름 설정 및 초기화
+    BLEDevice::init("Birdly_ESP32_Hardware");
+
+    // 2. 전파 송신 및 서비스 생성을 위한 BLE 서버 생성
+    BLEServer *pServer = BLEDevice::createServer();
+
+    // 3. 서버를 통한 서비스 생성
+    BLEService *pService = pServer->createService(SERVICE_UUID);
+
+    // 4. 서비스를 통한 특성 생성
+    BLECharacteristic *pCharacteristic = pService->createCharacteristic(CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_NOTIFY);
+
+    // 5. 서비스 개시
+    pService->start();
+
+    // 6. 공기 중에 전파를 쏴서 스마트폰 안테나가 탐지 가능하도록 함
+    pServer->getAdvertising()->start();
+}
+
+void loop()
+{
+    // 스마트폰 기기로부터 날라오는 전파는 loop가 아닌 백그라운드 이벤트가 처리함
+}
