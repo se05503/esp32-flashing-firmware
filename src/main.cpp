6dd1c25647c5aff5faeec3f657e12e96c933ff51
se05503 <seyeongb18@gmail.com>
2026-09-14 17:36:16 +0900

    feat: ESP32 → 클라이언트 Notify 구조 - Characteristic에 0x2092 UUID Descriptor 부착 - 클라이언트(스마트폰 앱)가 Descriptor 메모리에 Write할 때마다 호출될 콜백 정의 - Descriptor에 콜백 등록 - 콜백에서 읽은 메모리 값이 0x0001인 경우 → Characteristic에 데이터 주소 및 크기 설정 후 notify()로 0.1초 주기마다 송신

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,12 @@
+#include <Arduino.h>
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
+#include <BLE2902.h>
 
 // 기기간 연결 후 데이터 송수신을 위한 GATT 고유번호
 #define SERVICE_UUID "4fa1c432-132e-4b77-b952-b91b34c25638"
 #define CHARACTERISTIC_UUID "6e400002-b5a3-f393-e0a9-e50e24dcca9e"
-
-class PacketCallback : public BLECharacteristicCallbacks
-{
-    void onWrite(BLECharacteristic *pCharacteristic)
-    {
-        // 스마트폰이 쓴 바이트 데이터 가져오기
-        uint8_t *pData = pCharacteristic->getData();
-        size_t length = pCharacteristic->getLength();
-        if (length > 0)
-        {
-            // 수신된 데이터가 있는 경우 첫번째 바이트 데이터(명령) 꺼내오기
-            uint8_t command = pData[0];
-        }
-    }
-};
 
 void setup()
 {
@@ -32,9 +19,16 @@ void setup()
     // 3. 서버를 통한 서비스 생성
     BLEService *pService = pServer->createService(SERVICE_UUID);
 
-    // 4. 서비스를 통한 특성 생성 + 특성에 데이터를 받아올 콜백 등록
+    // 4. 서비스를 이용한 특성 생성 및 갖가지 속성 설정
     BLECharacteristic *pCharacteristic = pService->createCharacteristic(CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_NOTIFY);
+
+    // Write
     pCharacteristic->setCallbacks(new PacketCallback());
+
+    // Notify
+    BLE2902 *pDescriptor = new BLE2902();
+    pCharacteristic->addDescriptor(pDescriptor);          // 특성에 데이터 구독 ON/OFF 스위치 달기
+    pDescriptor->setCallbacks(new DescriptorCallbacks()); // ON/OFF 스위치 상태를 실시간으로 받을 콜백 등록
 
     // 5. 서비스 개시
     pService->start();
@@ -45,5 +39,16 @@ void setup()
 
 void loop()
 {
-    // 스마트폰 기기로부터 날라오는 전파는 loop가 아닌 백그라운드 이벤트가 처리함
+    // 스마트폰 기기로부터 날라오는 전파는 loop가 아닌 백그라운드 이벤트가 처리함 (Write)
+    // loop에서는 스마트폰으로 스트리밍 데이터를 송신함 (Notify)
+    bool isDeviceConnected = false; // 나중 구현
+
+    if (isDeviceConnected && isNotifySubscribed)
+    {
+        uint8_t sensorData = 50;                   // 임시 설정
+        pCharacteristic->setValue(&sensorData, 1); // 데이터 주소와 바이트 크기 전달
+        pCharacteristic->notify();                 // ESP32 → 클라이언트 데이터 푸시
+
+        delay(100); // 0.1초(100ms) 대기
+    }
 }
