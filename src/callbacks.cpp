94d78d48aace613127dc6cee87d73c72fdfd12d8
se05503 <seyeongb18@gmail.com>
2026-09-18 15:21:12 +0900

    refactor: Descriptor 콜백 Notify 활성화 여부 체크 개선 - Notify 외에 Indicate도 함께 고려 - 비트 마스크 기반 AND 연산자 활용 → Bit0 값으로 Notify 활성화 여부 파악

--- src/callbacks.cpp
+++ src/callbacks.cpp
@@ -34,16 +34,27 @@ void RxCallback::onWrite(BLECharacteristic *pCharacteristic)
     }
 }
 
+// 0x2902 Descriptor (CCCD) 콜백 → Notify 전용
 bool isNotifySubscribed = false; // 변수 실제 정의(초기화) 및 메모리 할당
 
 void DescriptorCallback::onWrite(BLEDescriptor *pDescriptor)
 {
     // 스마트폰 UI 버튼 클릭 → Flutter의 setNotifyValue(bool) 호출 → 0x2902 Descriptor 메모리 값(16비트) 0x0000 ↔ 0x0001 쓰기(Write) → 콜백함수 호출
-    uint8_t *value = pDescriptor->getValue();
-    if (value[0] == 1)
+    uint8_t *val = pDescriptor->getValue();
+    size_t length = pDescriptor->getLength(); // 단위: 바이트
+
+    if (length >= 2)
     {
-        isNotifySubscribed = true; // 초기화가 아니라 이미 정의되어 있는 변수에 값 대입하여 상태 변경
-        // setNotify(true) → 0x0001 Write → Switch ON
+        // BLE의 스위치 설정값(CCCD)은 2바이트 크기의 메모리 공간을 차지함
+        // 2바이트 데이터를 전송할 때, 이를 1바이트씩 2개로 쪼개어 배열 형태로 보낸다 → [첫 번째 바이트, 두 번째 바이트]
+        // val[0]: Bit0 ~ Bit7, val[1]: Bit8 ~ Bit15
+        // Bit0 = Notify, Bit1 = Indicate(ACK 포함)
+        // Bit0의 값만 확인하여 Notify 활성화 여부 체크
+        if (val[0] & 0x01)
+        {
+            isNotifySubscribed = true; // 초기화가 아니라 이미 정의되어 있는 변수에 값 대입하여 상태 변경
+            // setNotify(true) → 0x0001 Write → Switch ON
+        }
     }
     else
     {
